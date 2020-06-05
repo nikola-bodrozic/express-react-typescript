224ed715ca37c0add451e96ed21054d2f881fc32
Nikola <nikola@example.com>
2020-06-05 08:06:50 +0200

    backend update

--- nodejs/app.js
+++ nodejs/app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3001;
 
+const users = [
+   { id: 1, name: "ronald" },
+   { id: 2, name: "jacob" }
+ ];
+
 app.get("/", (req, res) =>
   res.json(
     { "msg": "welcome to API" }
@@ -9,11 +14,19 @@ app.get("/", (req, res) =>
 );
 
 app.get("/users", (req, res) =>
-  res.json([
-    { id: 1, name: "ronald" },
-    { id: 2, name: "jacob" }
-  ])
+  res.json(users)
 );
+
+app.get("/users/:id", (req, res) => {
+		const result = getUser(req.params.id)
+		res.json(result)
+	}
+);
+
+getUser = (id) => {
+	if (id > 2) return {"msg": "user doesn't exist"}
+	return users[id-1];
+}
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
